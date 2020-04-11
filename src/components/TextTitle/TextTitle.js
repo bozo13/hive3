0fe3dbf6b120f4675cdc57ed7702b48939818c31
Bela <keil@cadman.de>
2020-04-11 17:32:51 +0200

    second  mobile

--- src/components/TextTitle/TextTitle.js
+++ src/components/TextTitle/TextTitle.js
@@ -3,32 +3,15 @@ import { Typography, Theme, Grid,Box, Container } from "@material-ui/core"
 import Styles from './TextTitle.module.scss'
 import BgTop from '../../assets/images/BgTop.png'
 
-const styles = {
-  root: {
-      width:'100vw',
-      height:'38vh',
-      backgroundImage: `url(${BgTop})`,
-      backgroundSize: 'cover',
-      backgroundPosition: 'center bottom',
-      zIndex: '2',
-      position: 'absolute',
-      left: '0%',
-      backgroundRepeat: 'no-repeat',
-      marginTop: '0',
-  },
-  TextTitle:{
 
-  },
-  TextTitle2:{
-  },
-}
 
 const TextTitle = ({ text ,texttitle2}) => {
 
   return (
+  <Box className={Styles.root}>
    <Container >
     <Grid container className={Styles.TitleWrap}>
-      <Box component={Grid} item xs={2} display={{ xs: "none", sm: "block" }} />
+      <Box component={Grid} item xs={2} display={{ xs: "none", sm: "block" }} gir />
 
     <Grid item xs={12} lg={8}>
        <Typography variant='h2' className={Styles.FirstTitle}>{text} </Typography>
@@ -44,6 +27,7 @@ const TextTitle = ({ text ,texttitle2}) => {
       ></Box>
       </Grid>
     </Container>
+  </Box>
   )
 }
 
